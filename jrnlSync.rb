f83aa82da5ba4a832f431ed88f6f6959cb803805
jm96441n <john@johnmaguiredeveloper.com>
2021-11-27 19:23:31 -0500

    Brew formula update for jrnlSync version v1.0.3

--- jrnlSync.rb
+++ jrnlSync.rb
@@ -5,21 +5,21 @@
 class JrnlSync < Formula
   desc "Faciliatates automatic sync of jrnl notes to external sources"
   homepage "https://www.github.com/jm96441n/jrnlSync"
-  version "1.0.2"
+  version "1.0.3"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "http://github.com/jm96441n/jrnlSync/releases/v1.0.2/jrnlSync_1.0.2_Darwin_arm64.tar.gz"
-      sha256 "8ae05853b4190b450931080c26d40811bd5fa5346d51ad67bb097f1e78c99f9e"
+      url "http://github.com/jm96441n/jrnlSync/releases/v1.0.3/jrnlSync_1.0.3_Darwin_arm64.tar.gz"
+      sha256 "4f5157adab0752f07508f7a2a37c6b719df2d4c9274fe2a152c07c24028f6e9d"
 
       def install
         bin.install "jrnlSync"
       end
     end
     if Hardware::CPU.intel?
-      url "http://github.com/jm96441n/jrnlSync/releases/v1.0.2/jrnlSync_1.0.2_Darwin_x86_64.tar.gz"
-      sha256 "42c05126c3950ea5c3a18b58c27178d726b885e73a3600170a841d56716c624f"
+      url "http://github.com/jm96441n/jrnlSync/releases/v1.0.3/jrnlSync_1.0.3_Darwin_x86_64.tar.gz"
+      sha256 "bcb60b5087cea8505c76bd53f5e3aa8addc58a8fb2cd227d38e311d8b3070927"
 
       def install
         bin.install "jrnlSync"
@@ -29,16 +29,16 @@ class JrnlSync < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "http://github.com/jm96441n/jrnlSync/releases/v1.0.2/jrnlSync_1.0.2_Linux_arm64.tar.gz"
-      sha256 "9f3aa6974e6b6ee53eeba3c42c02452f508dc5708dc667719dc5d33ae42f21cc"
+      url "http://github.com/jm96441n/jrnlSync/releases/v1.0.3/jrnlSync_1.0.3_Linux_arm64.tar.gz"
+      sha256 "a6cb5450719850e2a1141347cff1ad5fc1a21abe6af55340e55e1a0a51bf996e"
 
       def install
         bin.install "jrnlSync"
       end
     end
     if Hardware::CPU.intel?
-      url "http://github.com/jm96441n/jrnlSync/releases/v1.0.2/jrnlSync_1.0.2_Linux_x86_64.tar.gz"
-      sha256 "93137b5e0cc20d84a9c7b34c8d62f1e16d8c5efc1f4975102df4e029440437c0"
+      url "http://github.com/jm96441n/jrnlSync/releases/v1.0.3/jrnlSync_1.0.3_Linux_x86_64.tar.gz"
+      sha256 "a20bc5c466b58d42594f38d4c4b9151fbf331b23ef92906d2e6a778770ef54ad"
 
       def install
         bin.install "jrnlSync"
